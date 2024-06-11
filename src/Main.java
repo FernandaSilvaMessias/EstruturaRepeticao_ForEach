c86892c53541fb0d8318ba7c270fe399a62b0579
CassiaFernandaMessias <cassia.fernanda.dev@gmail.com>
2024-06-10 22:59:00 -0300

    ForEach

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String alunos []= {"Felipe", "Jonas","Júlia","Marcos" };
+        for (String aluno : alunos){
+            System.out.println("Nome do aluno é " + aluno);
+        }
+    }
+}
